66e97075810836dc236372adcc6930987eda3516
const myNickname = "Oni <onurfistikci@mail.de>
2024-01-16 15:30:32 +0100

    Update pages/api/projects/index.js
    Co-authored-by: CJAkkuec <90191489+CJAkkuec@users.noreply.github.com>

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -11,7 +11,6 @@ export default async function handler(request, response) {
   if (request.method === "POST") {
     try {
       const projectData = request.body;
-      console.log("in api: ", projectData);
       await Project.create(projectData);
       return response.status(201).json({ status: "Project created" });
     } catch (error) {
