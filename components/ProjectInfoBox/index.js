a273430ce23ab96711da81b97db4fb84697f68d1
onur fistikci <onurfistikci@mail.de>
2024-01-31 12:17:54 +0100

    add styling to detail page

--- components/ProjectInfoBox/index.js
+++ components/ProjectInfoBox/index.js
@@ -1,16 +1,21 @@
 import styled from "styled-components";
 
 const StyledBox = styled.div`
-  background-color: #e8e8e8;
+  background-color: #e8e8e8; 
   border-radius: 20px;
-  padding: 5px;
+  padding: 20px;
   margin: 10px;
+`;
+
+const SectionTitles = styled.h2`
+ margin-top: 0;
+  font-size: 1.2rem;
 `;
 
 export default function ProjectInfoBox({ title, text, children }) {
   return (
     <StyledBox>
-      <h2>{title}:</h2>
+      <SectionTitles>{title} :</SectionTitles>
       <p>{text}</p>
       {children}
     </StyledBox>
