dff38ba2f80bcd19e03d9c233766084587d80ad2
kristinwegener <kristinwegener94@gmail.com>
2024-02-01 16:05:09 +0100

    style article card

--- components/ArticleCard/index.js
+++ components/ArticleCard/index.js
@@ -2,21 +2,26 @@ import Image from "next/image";
 import styled from "styled-components";
 
 const StyledArticleCard = styled.div`
-  display: flex;
+padding: 10px;
   margin: 10px;
   background-color: #858f87;
   color: white;
   border-radius: 10px;
-  gap: 10px;
   align-items: center;
   overflow: hidden;
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${(
+    props
+  ) => props.image});
+  background-size: cover;
+
+}
 `;
 
 export default function ArticleCard({ article }) {
   return (
-    <StyledArticleCard>
-      <Image src={article.image} height="100" width="100" alt="article image" />
+    <StyledArticleCard image={article.image}>
       <h3>{article.title}</h3>
+      <p>{article.body.slice(0, 100)}...</p>
     </StyledArticleCard>
   );
 }
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,22 +1,3 @@
-// export default function ProjectList({
-//     projectsToDisplay,
-//     favorites,
-//     onToggleFavorite,
-//   }) {
-//     return (
-//       <StyledProjectList>
-//         {projectsToDisplay.map((project) => (
-//           <Card
-//             key={project._id}
-//             project={project}
-//             favorites={favorites}
-//             onToggleFavorite={onToggleFavorite}
-//           />
-//         ))}
-//       </StyledProjectList>
-//     );
-//   }
-
 import ArticleCard from "@/components/ArticleCard";
 
 export default function KnowledgeOverview({ articles }) {
