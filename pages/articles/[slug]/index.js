8701524e6cd916fc68b0f0e4dfd9fd1943700ce9
kristinwegener <kristinwegener94@gmail.com>
2024-02-02 14:08:42 +0100

    show project cards in articles

--- pages/articles/[slug]/index.js
+++ pages/articles/[slug]/index.js
@@ -1,28 +1,34 @@
-import { useRouter } from 'next/router';
-import useSWR from 'swr';
-import LoadingSpinner from '@/components/LoadingSpinner';
-import Image from 'next/image';
-import ReactMarkdown from 'react-markdown';
-import Link from 'next/link';
-import styled from 'styled-components';
-import BackArrowIcon from '@/components/ArticleCard/BackArrowIcon';
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import Image from "next/image";
+import ReactMarkdown from "react-markdown";
+import Link from "next/link";
+import styled from "styled-components";
+import BackArrowIcon from "@/components/ArticleCard/BackArrowIcon";
+import Card from "@/components/Card";
+import ProjectList from "@/components/ProjectList";
 
 const ArticleImage = styled(Image)`
-width: 100%;
-height: auto;
+  width: 100%;
+  height: auto;
 `;
 
 const ArticleTextContent = styled.div`
-width: 90%;
-margin: auto;
+  width: 90%;
+  margin: auto;
 `;
 
+const StyledSubheading = styled.h3`
+  text-align: center;
+  margin-top: 40px;
+`;
 
-export default function ArticlePage() {
-const router = useRouter();
-const { slug } = router.query;
+export default function ArticlePage({ projects }) {
+  const router = useRouter();
+  const { slug } = router.query;
 
-const {
+  const {
     data: article,
     isLoading,
     error,
@@ -34,18 +40,33 @@ const {
     router.push("/404");
     return;
   }
+
+  const projectsOfSameCategory = projects
+    .filter((project) => project.category === article.category)
+    .slice(0, 3);
+
   return (
     <div>
-    <Link href="/articles"><BackArrowIcon/></Link>
-      <ArticleImage src={article.image} alt={article.title} width={0}
+      <Link href="/articles">
+        <BackArrowIcon />
+      </Link>
+      <ArticleImage
+        src={article.image}
+        alt={article.title}
+        width={0}
         height={0}
         sizes="100vw"
-/>
+      />
       <ArticleTextContent>
-      <h2>{article.title}</h2>
-      <ReactMarkdown>{article.body}</ReactMarkdown>
+        <h2>{article.title}</h2>
+        <ReactMarkdown>{article.body}</ReactMarkdown>
       </ArticleTextContent>
-      <Link href="/articles"><BackArrowIcon/></Link>
+      <StyledSubheading>Related Projects:</StyledSubheading>
+      <ProjectList projectsToDisplay={projectsOfSameCategory} />
+
+      <Link href="/articles">
+        <BackArrowIcon />
+      </Link>
     </div>
   );
 }
