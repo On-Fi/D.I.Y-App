084558ce97dd16eba38a91a663dc377a3db3785a
kristinwegener <kristinwegener94@gmail.com>
2024-02-05 16:40:56 +0100

    enable deletion of steps, materials, tools

--- components/Form/index.js
+++ components/Form/index.js
@@ -17,10 +17,10 @@ const StyledForm = styled.form`
 `;
 
 const StyledInput = styled.input`
+  width: 100%;
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 20px;
-  margin-bottom: 20px;
   &:focus {
     outline: 2px solid ${(props) => themes[props.theme].primaryButtonColor};
   }
@@ -50,6 +50,7 @@ const UploadSection = styled.div`
 
 const StyledStep = styled.div`
   display: flex;
+  align-items: center;
   gap: 10px;
 `;
 
@@ -82,6 +83,18 @@ const RadioInputContainer = styled.div`
   margin-bottom: 20px;
 `;
 
+const CategoryContainer = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 export default function Form({
   onSubmit,
   onCancel,
@@ -100,6 +113,7 @@ export default function Form({
   const [material, setMaterial] = useState(
     project.material || [{ id: uuidv4(), amount: "", name: "" }]
   );
+  const [time, setTime] = useState("24");
 
   const uploadImage = async () => {
     const formData = new FormData();
@@ -126,6 +140,10 @@ export default function Form({
     onSubmit(projectData);
   }
 
+  function onDurationChange(event) {
+    setTime(event.target.value);
+  }
+
   function handleAddStep() {
     const lastStep = steps[steps.length - 1];
     if (!lastStep || lastStep.text.trim() !== "") {
@@ -133,6 +151,11 @@ export default function Form({
     } else {
       alert("Please fill in the previous step first.");
     }
+  }
+
+  function handleStepRemoval(id) {
+    console.log(id);
+    setSteps(steps.filter((step) => step.id !== id));
   }
 
   function handleInstructionInput(text, id) {
@@ -160,6 +183,10 @@ export default function Form({
     );
   }
 
+  function handleToolRemoval(id) {
+    setTools(tools.filter((tool) => tool.id !== id));
+  }
+
   function handleAddMaterial() {
     const lastMaterial = material[material.length - 1];
     if (!lastMaterial || lastMaterial.name.trim() !== "") {
@@ -183,230 +210,240 @@ export default function Form({
     );
   }
 
+  function handleMaterialRemoval(id) {
+    setMaterial(material.filter((item) => item.id !== id));
+  }
+
   return (
     <>
       <StyledForm onSubmit={handleSubmit}>
-        <label htmlFor="title">Title:*</label>
-        <StyledInput
-          theme={theme}
-          type="text"
-          id="title"
-          name="title"
-          defaultValue={project.title}
-          required
-        />
-
-        <label htmlFor="image">Image:</label>
-        <UploadSection>
-          <UploadPreview
-            src={imageId}
-            width="300"
-            crop="scale"
-            alt="beispiel image"
-          />
-          <UploadButtonSection>
-            <input
-              type="file"
-              name="image"
-              title="image"
-              onChange={(event) => setImageSelected(event.target.files[0])}
-            />
-            <button type="button" onClick={uploadImage}>
-              Upload an Image
-            </button>
-          </UploadButtonSection>
-        </UploadSection>
-
-        <label htmlFor="category">Category:* </label>
-        <StyledSelect
-          theme={theme}
-          defaultValue={project.category || ""}
-          name="category"
-          id="category"
-          required
-        >
-          <option value="" disabled selected>
-            choose here
-          </option>
-          <option value="home">home</option>
-          <option value="garden">garden</option>
-          <option value="fashion">fashion</option>
-          <option value="kitchen">kitchen</option>
-          <option value="bathroom">bathroom</option>
-        </StyledSelect>
-
-        {/* <label htmlFor="difficulty">Difficulty:*</label>
-        <RadioInputContainer>
-          <label>
-            <RadioInput
-              theme={theme}
-              type="radio"
-              value="beginner"
-              name="difficulty"
-              id="beginner"
-              required
-              defaultChecked={project.difficulty === "beginner"}
-            />
-            beginner
-          </label>
-          <label>
-            <RadioInput
-              theme={theme}
-              type="radio"
-              value="advanced"
-              name="difficulty"
-              id="advanced"
-              required
-              defaultChecked={project.difficulty === "advanced"}
-            />
-            advanced
-          </label>
-          <label>
-            <RadioInput
-              theme={theme}
-              type="radio"
-              value="expert"
-              name="difficulty"
-              id="expert"
-              required
-              defaultChecked={project.difficulty === "expert"}
-            />
-            expert
-          </label>
-        </RadioInputContainer> */}
-
-        <label htmlFor="difficulty">Difficulty:*</label>
-        <RadioInputContainer>
-          {["beginner", "advanced", "expert"].map((entry) => (
-            <label key={entry}>
-              <RadioInput
-                theme={theme}
-                type="radio"
-                value={entry}
-                name="difficulty"
-                id={entry}
-                required
-                defaultChecked={project.difficulty === entry}
-              />
-              {entry}
-            </label>
-          ))}
-        </RadioInputContainer>
-
-        <label htmlFor="priceCategory">Price category:*</label>
-        <RadioInputContainer>
-          {["€", "€€", "€€€"].map((entry) => (
-            <label key={entry}>
-              <RadioInput
-                theme={theme}
-                type="radio"
-                value={entry}
-                name="priceCategory"
-                id={entry}
-                required
-                defaultChecked={project.priceCategory === entry}
-              />
-              {entry}
-            </label>
-          ))}
-        </RadioInputContainer>
-
-        <label htmlFor="time">Duration:* </label>
-        <StyledInput
-          theme={theme}
-          type="range"
-          id="time"
-          name="time"
-          min="0"
-          max="48"
-          defaultValue={project.time || 24}
-        />
-
-        <label htmlFor="tools">Tools:*</label>
-        {tools.map((tool) => (
+        <CategoryContainer>
+          <label htmlFor="title">Title:*</label>
           <StyledInput
             theme={theme}
-            key={tool.id}
-            id="tool"
-            name="tool"
-            defaultValue={tool.name}
-            placeholder='e.g. "hammer"'
-            onInput={(event) => handleToolInput(event.target.value, tool.id)}
+            type="text"
+            id="title"
+            name="title"
+            defaultValue={project.title}
             required
-          ></StyledInput>
-        ))}
-        <Button
-          theme={theme}
-          color="secondary"
-          type="button"
-          onClick={handleAddTool}
-        >
-          add tool
-        </Button>
+          />
+        </CategoryContainer>
 
-        <label htmlFor="tools">Material:*</label>
-        {material.map((item) => (
-          <MaterialInput key={item.id}>
-            <StyledInput
-              theme={theme}
-              type="number"
-              min="1"
-              id="material"
-              name="material"
-              placeholder="e.g. 5"
-              defaultValue={item.amount}
-              onInput={(event) =>
-                handleMaterialAmountInput(event.target.value, item.id)
-              }
-              required
+        <CategoryContainer>
+          <label htmlFor="image">Image:</label>
+          <UploadSection>
+            <UploadPreview
+              src={imageId}
+              width="300"
+              crop="scale"
+              alt="beispiel image"
             />
-            <StyledInput
-              theme={theme}
-              key={item.id}
-              id="material"
-              name="material"
-              defaultValue={item.name}
-              placeholder="nails"
-              onInput={(event) =>
-                handleMaterialNameInput(event.target.value, item.id)
-              }
-              required
-            />
-          </MaterialInput>
-        ))}
-        <Button
-          theme={theme}
-          color="secondary"
-          type="button"
-          onClick={handleAddMaterial}
-        >
-          add material
-        </Button>
+            <UploadButtonSection>
+              <input
+                type="file"
+                name="image"
+                title="image"
+                onChange={(event) => setImageSelected(event.target.files[0])}
+              />
+              <button type="button" onClick={uploadImage}>
+                Upload an Image
+              </button>
+            </UploadButtonSection>
+          </UploadSection>
+        </CategoryContainer>
 
-        <label htmlFor="instructions">Instructions:*</label>
-        {steps.map((step, index) => (
-          <StyledStep key={step.id}>
-            <label>Step {index + 1} </label>
-            <StyledInput
-              theme={theme}
-              id="instructions"
-              name="instructions"
-              defaultValue={step.text}
-              onInput={(event) =>
-                handleInstructionInput(event.target.value, step.id)
-              }
-              required
-            ></StyledInput>
-          </StyledStep>
-        ))}
-        <Button
-          theme={theme}
-          color="secondary"
-          type="button"
-          onClick={handleAddStep}
-        >
-          add step
-        </Button>
+        <CategoryContainer>
+          <label htmlFor="category">Category:* </label>
+          <StyledSelect
+            theme={theme}
+            defaultValue={project.category || ""}
+            name="category"
+            id="category"
+            required
+          >
+            <option value="" disabled selected>
+              choose here
+            </option>
+            <option value="home">home</option>
+            <option value="garden">garden</option>
+            <option value="fashion">fashion</option>
+            <option value="kitchen">kitchen</option>
+            <option value="bathroom">bathroom</option>
+          </StyledSelect>
+        </CategoryContainer>
+
+        <CategoryContainer>
+          <label htmlFor="difficulty">Difficulty:*</label>
+          <RadioInputContainer>
+            {["beginner", "advanced", "expert"].map((entry) => (
+              <label key={entry}>
+                <RadioInput
+                  theme={theme}
+                  type="radio"
+                  value={entry}
+                  name="difficulty"
+                  id={entry}
+                  required
+                  defaultChecked={project.difficulty === entry}
+                />
+                {entry}
+              </label>
+            ))}
+          </RadioInputContainer>
+        </CategoryContainer>
+
+        <CategoryContainer>
+          <label htmlFor="priceCategory">Price category:*</label>
+          <RadioInputContainer>
+            {["€", "€€", "€€€"].map((entry) => (
+              <label key={entry}>
+                <RadioInput
+                  theme={theme}
+                  type="radio"
+                  value={entry}
+                  name="priceCategory"
+                  id={entry}
+                  required
+                  defaultChecked={project.priceCategory === entry}
+                />
+                {entry}
+              </label>
+            ))}
+          </RadioInputContainer>
+        </CategoryContainer>
+
+        <CategoryContainer>
+          <label htmlFor="time">Duration: {time} hours</label>
+          <StyledInput
+            theme={theme}
+            type="range"
+            id="time"
+            name="time"
+            min="0"
+            max="48"
+            defaultValue={project.time || 24}
+            onChange={onDurationChange}
+          />
+        </CategoryContainer>
+
+        <CategoryContainer>
+          <label htmlFor="tools">Tools:*</label>
+          {tools.map((tool) => (
+            <InputContainer key={tool.id}>
+              <StyledInput
+                theme={theme}
+                id="tool"
+                name="tool"
+                defaultValue={tool.name}
+                placeholder='e.g. "hammer"'
+                onInput={(event) =>
+                  handleToolInput(event.target.value, tool.id)
+                }
+                required
+              />
+              <Button
+                theme={theme}
+                type="button"
+                color="secondary"
+                onClick={() => handleToolRemoval(tool.id)}
+              >
+                x
+              </Button>
+            </InputContainer>
+          ))}
+          <Button
+            theme={theme}
+            color="secondary"
+            type="button"
+            onClick={handleAddTool}
+          >
+            add tool
+          </Button>
+        </CategoryContainer>
+
+        <CategoryContainer>
+          <label htmlFor="material">Material:*</label>
+          {material.map((item) => (
+            <MaterialInput key={item.id}>
+              <StyledInput
+                theme={theme}
+                type="number"
+                min="1"
+                id="material"
+                name="material"
+                placeholder="e.g. 5"
+                defaultValue={item.amount}
+                onInput={(event) =>
+                  handleMaterialAmountInput(event.target.value, item.id)
+                }
+                required
+              />
+              <StyledInput
+                theme={theme}
+                key={item.id}
+                id="material"
+                name="material"
+                defaultValue={item.name}
+                placeholder="nails"
+                onInput={(event) =>
+                  handleMaterialNameInput(event.target.value, item.id)
+                }
+                required
+              />
+              <Button
+                type="button"
+                theme={theme}
+                color="secondary"
+                onClick={() => handleMaterialRemoval(item.id)}
+              >
+                x
+              </Button>
+            </MaterialInput>
+          ))}
+          <Button
+            theme={theme}
+            color="secondary"
+            type="button"
+            onClick={handleAddMaterial}
+          >
+            add material
+          </Button>
+        </CategoryContainer>
+
+        <CategoryContainer>
+          <label htmlFor="instructions">Instructions:*</label>
+          {steps.map((step, index) => (
+            <StyledStep key={step.id}>
+              <label>{index + 1}.</label>
+              <StyledInput
+                theme={theme}
+                id="instructions"
+                name="instructions"
+                defaultValue={step.text}
+                onInput={(event) =>
+                  handleInstructionInput(event.target.value, step.id)
+                }
+                required
+              ></StyledInput>
+              <Button
+                type="button"
+                theme={theme}
+                color="secondary"
+                onClick={() => handleStepRemoval(step.id)}
+              >
+                x
+              </Button>
+            </StyledStep>
+          ))}
+          <Button
+            theme={theme}
+            color="secondary"
+            type="button"
+            onClick={handleAddStep}
+          >
+            add step
+          </Button>
+        </CategoryContainer>
 
         <Button theme={theme} type="submit">
           Save
