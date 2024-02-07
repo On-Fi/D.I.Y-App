a38014c9ccfd7df43556029179f5ca54a21f8119
Andreas Gottwald <andreas.gottwald@hotmail.de>
2024-02-07 13:13:26 +0100

    add credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,36 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "newWorm",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "newWormPassword",
+        },
+      },
+      async authorize() {
+        return {
+          id: 1,
+          name: "WoodWormGuest",
+          email: "new@WoodWorm.com",
+          image: "/wurm.png",
+        };
+      },
     }),
   ],
 };
