b65504b76154c87cd9b8d38ce303fd9bd595a6ae
onur fistikci <onurfistikci@mail.de>
2024-02-07 10:47:58 +0100

    add color coding to cards

--- components/ShortFactsBox/index.js
+++ components/ShortFactsBox/index.js
@@ -11,7 +11,7 @@ const FactBox = styled.div`
   justify-content: center;
 `;
 const ShortFact = styled.span`
-  background-color: ${(props) => themes[props.theme].primaryButtonColor};
+  background-color: ${(props) => props.isWhite ? '#FFFFFF' : themes[props.theme].primaryButtonColor};
   color: ${(props) => props.color === "primary" ? themes[props.theme].primaryButtonTextColor : themes[props.theme].secondaryButtonTextColor};
   border-radius: 20px;
   padding: 5px;
@@ -19,13 +19,13 @@ const ShortFact = styled.span`
   padding-right: 10px;
 `;
 
-export default function ShortFactsBox({ project, theme }) {
+export default function ShortFactsBox({ project, theme, isWhite }) {
   return (
     <FactBox>
-      <ShortFact theme= {theme} color = "primary">{project.time} hours</ShortFact>
-      <ShortFact theme= {theme} color = "primary">{project.priceCategory}</ShortFact>
-      <ShortFact theme= {theme} color = "primary">{project.category}</ShortFact>
-      <ShortFact theme= {theme} color = "primary">{project.difficulty}</ShortFact>
+      <ShortFact theme= {theme} isWhite={isWhite} color = "primary">{project.time} hours</ShortFact>
+      <ShortFact theme= {theme} isWhite={isWhite} color = "primary">{project.priceCategory}</ShortFact>
+      <ShortFact theme= {theme} isWhite={isWhite} color = "primary">{project.category}</ShortFact>
+      <ShortFact theme= {theme} isWhite={isWhite} color = "primary">{project.difficulty}</ShortFact>
     </FactBox>
   );
 }
